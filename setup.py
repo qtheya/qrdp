33f81bb26ae3484713cbab8986352599bc962727
Alexander Kuzmovych <qtheya@gmail.com>
2014-11-18 15:19:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/bin/bash
 user=`whoami`
-path="/home/"$user"/qrdp"
+path="/home/"$user"/.qrdp"
 mkdir $path
 touch /home/$user/.local/share/applications/qrdp.desktop
 printf "[Desktop Entry]\nVersion=1.0\nName=qrdp\nComment=qt5 gui for rdesktop\nExec="$path"/qrdp.py\nIcon="$path"/icon.png\nPath=/h    ome/qtheya/.qrdp/\nTerminal=false\nType=Application\nEncoding=UTF-8\nCategories=Utility;Network;Application;" > /home/$user/.local/    share/applications/qrdp.desktop
